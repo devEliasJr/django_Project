5e509c4a0808b013e8267a55d222780ad71ab2d3
develias <eliasengmecanica@gmail.com>
2023-01-09 17:17:59 -0300

    Fim estrutura basica

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -20,6 +23,10 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
